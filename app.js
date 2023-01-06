15f9fff4444db231f9c4f3b6d7db3b72bd86790d
Juan Bove <juansbove@gmail.com>
2023-01-05 23:42:26 -0300

    Agregue modulos a package.json y modifique app.js para poder levantar el proyecto con Render

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.listen(3000, () =>
-    console.log('Servidor corriendo en puerto 3000'));
+const port = process.env.PORT || 3001;
+app.listen(port, () =>
+    console.log(`Servidor corriendo en puerto ${port}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname + '/views/home.html'))
